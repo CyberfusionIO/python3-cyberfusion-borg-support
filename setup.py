e5b5915af279758563b00c555875bab200a02771
William Edwards <wedwards@cyberfusion.nl>
2022-10-04 17:15:03 +0200

    Rename python-cyberfusion-borg-support to python3-cyberfusion-borg-support

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(
-    name="python-cyberfusion-borg-support",
-    version="1.3.7.2",
+    name="python3-cyberfusion-borg-support",
+    version="1.3.7.2.1",
     description="BorgSupport Python library/tools",
     author="William Edwards",
     author_email="wedwards@cyberfusion.nl",
-    url="https://vcs.cyberfusion.nl/cyberfusion/python-cyberfusion-borg-support",
+    url="https://vcs.cyberfusion.nl/cyberfusion/python3-cyberfusion-borg-support",
     license="Closed",
     packages=[
         "cyberfusion.BorgSupport",
