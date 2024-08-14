7019b8622a549e309bb924d172bda465b1f5a19b
William Edwards <wedwards@cyberfusion.nl>
2024-08-14 21:00:06 +0200

    Make setup.py consistent across all projects

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ setup(
     description="Library for Borg.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.9",
-    author="William Edwards",
-    author_email="wedwards@cyberfusion.nl",
+    author="Cyberfusion",
+    author_email="support@cyberfusion.io",
     url="https://github.com/CyberfusionIO/python3-cyberfusion-borg-support",
     platforms=["linux"],
     packages=[
@@ -23,11 +22,4 @@ setup(
     data_files=[],
     package_dir={"": "src"},
     install_requires=["cached_property==1.5.2"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    keywords=["cyberfusion", "borg", "borgbackup"],
-    license="MIT",
 )
